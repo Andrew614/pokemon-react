0f2cc63dea93a0f968686a47ebc72a3979d5f158
Andrew614 <azhang213@gmail.com>
2019-09-05 01:01:13 -0400

    add css styling

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import './Pokecard.css'
 
 const Pokecard = ({ id, name, type, base_experience }) => {
     const pokemonId = ("00" + id).slice(-3)
     const imgSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonId}.png`
-    console.log(name)
+
     return (
-        <>
+        <div className='Pokecard'>
             <h3 className='card__title'>{name}</h3>
             <img className='card__img' src={imgSrc} alt={name} />
             <ul className='card__list'>
                 <li className='card__list-item'>Type: {type}</li>
                 <li className='card__list-item'>EXP: {base_experience}</li>
             </ul>
-        </>
+        </div>
     )
 }
 
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 const Pokedex = ({ pokemonList }) => {
     return (
-        <div>
-            <h1>Pokedex</h1>
-            {pokemonList.map((pokemon) => (
-                <section className='card' key={pokemon.id}>
-                    <Pokecard id={pokemon.id}
-                        name={pokemon.name}
-                        type={pokemon.type}
-                        base_experience={pokemon.base_experience}
-                    />
-                </section>
-            ))}
+        <div className='Pokedex'>
+            <h1 className='title'>Pokedex</h1>
+            <div className='pokemon-list'>
+                {pokemonList.map((pokemon) => (
+                    <div className='card' key={pokemon.id}>
+                        <Pokecard id={pokemon.id}
+                            name={pokemon.name}
+                            type={pokemon.type}
+                            base_experience={pokemon.base_experience}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
